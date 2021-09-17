6f315005c67028ad948f5d906fce7db1ab28219b
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-09-18 00:22:09 +0430

    feat:custom permissions has been added

---
+++ backend/core/permissions.py
@@ -0,0 +1,34 @@
+from rest_framework.permissions import BasePermission, SAFE_METHODS
+
+# from django.contrib.auth.models import AnonymousUser
+
+
+class SampleCustomPermission(BasePermission):
+
+    """First Priority : has_permission() will be executed for the first time"""
+
+    def has_permission(self, request, view):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return True
+
+    # ONLY FOR A API VIEW THAT IS DEDICATED FOR A SPECIFIC OBJECT LIKE : RetrieveUpdateAPIView and not like : ListAPIView
+    """After the first priority the second priority that is has_object_permission() will be executed"""
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return True
+
+
+class Forbidden(BasePermission):
+
+    """First Priority : has_permission() will be executed for the first time"""
+
+    def has_permission(self, request, view):
+        """
+        Return `True` if permission is granted, `False` otherwise.
+        """
+        return False
--- backend/core/views.py
+++ backend/core/views.py
@@ -1,3 +1,4 @@
 from django.shortcuts import render
+from .generics import EnhancedModelViewSet
 
 # Create your views here.
