c075a7f4afbb076b5f9d96a653819710566ee772
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-10-03 22:45:11 +0330

    feat:accounts admin panel is created

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
 # Register your models here.
+@admin.register(User)
+class MyUserAdmin(UserAdmin):
+    pass
+    # list_display = ("user", "title", "business_phone")
+    # list_filter = ["user", "title", "business_phone"]
+    # search_fields = ('user__username','postal_code__startswith')
+    # prepopulated_fields = {'slug': ('title',)}
