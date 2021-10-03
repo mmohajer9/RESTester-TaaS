fd0d4eb9febf4917a05e296bbb510696f38c844c
Mohammad Mahdi Mohajer <mmohajer98@gmail.com>
2021-10-03 18:16:05 +0330

    feat:frontend routes

--- frontend/src/common/routes.js
+++ frontend/src/common/routes.js
@@ -1,6 +1,11 @@
 const routes = {
   homepage: '/',
   dashboard: '/dashboard',
+  testPlans: '/test-plans',
+  testPlanDetail: '/test-plans/:id',
+  testSuites: '/test-suites',
+  testSuiteDetail: '/test-suites/:id',
+
   auth: '/auth',
   api: {},
 };
